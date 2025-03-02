39f955c9754de4b0678577ebe09e56747ca647e7
Russell Helmstedter <rhelmstedter@gmail.com>
2025-03-02 13:27:58 -0800

    updated used cars

--- coding-activities/used_cars.py
+++ coding-activities/used_cars.py
@@ -2,44 +2,70 @@
 
 import json
 import csv
+from rich import print
 
-import requests
+# import requests
+import install_playwright
+
 
 from bs4 import BeautifulSoup
+import time
 
+from playwright.sync_api import sync_playwright
 
 URL = "https://www.autotrader.com/cars-for-sale/ventura-ca?newSearch=false&numRecords=100&zip=93001"
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
-cars = soup.find_all("div", class_="col-xs-12 display-flex col-sm-4")
-with open("used_cars.csv", mode="w") as used_car_db:
+try:
+    with open("used_cars.html", "r") as f:
+        html_content = f.read()
+
+except FileNotFoundError:
+    with sync_playwright() as p:
+        install_playwright.install(p.chromium)
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(URL)
+        # Scroll to the bottom of the page
+        for chunk in range(20):
+            page.mouse.wheel(0, 2000)
+            time.sleep(.1)
+        time.sleep(10)
+
+        # Get the HTML content of the page
+        html_content = page.content()
+
+        with open("used_cars.html", "w") as f:
+            f.write(html_content)
+        # Close the browser
+        browser.close()
+
+
+soup = BeautifulSoup(html_content, "html.parser")
+cars = soup.find_all("div", class_="col-xs-12 col-sm-4 display-flex")
+with open("used_cars.csv", mode="w", encoding="utf-8") as used_car_db:
     fieldnames = [
-        "brand",
+        "make",
         "model",
-        "color",
-        "mpg",
-        "fuel_type",
-        "full_name",
+        "year",
         "price",
         "mileage",
-        "year",
     ]
     car_writer = csv.DictWriter(used_car_db, fieldnames=fieldnames)
     for car in cars:
         try:
-            car_data = json.loads(car.find("script").text)
+            car_data = car.find("div", class_="inventory-listing-body padding-3")
+            car = car_data.find("h2").text
+            mileage = car_data.find("div", class_="text-bold text-subdued-lighter margin-top-3").text
+            price = car_data.find("div", class_="text-size-500 text-ultra-bold first-price").text
+            _, year, make, *model = car.split()
+
             fields = [
-                car_data["brand"]["name"]["name"],  # brand
-                car_data["model"]["name"],  # model
-                car_data["color"],  # color
-                car_data["fuelEfficiency"],  # mpg
-                car_data["fuelType"]["name"],  # fuel_type
-                car_data["name"],  # full_name
-                car_data["offers"]["price"],  # price
-                car_data["mileageFromOdometer"]["value"],  # mileage
-                car_data["productionDate"],  # year
+                make,
+                " ".join(model),
+                year,
+                int(mileage.split()[0].replace(",", "")),
+                float(price.replace(",", "")),
             ]
-        except AttributeError:
+        except AttributeError as e:
             pass
         car_writer.writerow({fn: f for fn, f in zip(fieldnames, fields)})
